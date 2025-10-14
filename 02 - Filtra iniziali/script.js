85418b205b8f992b756a863d796e8850150aec82
dianatoti <dianatoti10@gmail.com>
2025-10-14 19:59:30 +0200

    feat: eseguito 02 esercizio

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -2,12 +2,23 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+// @param {array}
+// @param {letter}
 
 // Dichiara la funzione qui.
-
-
+function StringEletter (array, letter) {
+let iniziali = [];
+for (let i = 0; i < array.length; i++) {
+let name = array[i];
+if (name [0] === letter) {
+    iniziali.push(name);
+}
+}
+return iniziali;
+}
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(StringEletter(names, "A"));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
