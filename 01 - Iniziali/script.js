8a376ff4a7104eb65d47469469bfd5350c555479
dianatoti <dianatoti10@gmail.com>
2025-10-14 19:37:25 +0200

    feat: svolto esercizio 01

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function iniziali(names) {
+    let iniziali = [];
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i];
+    let iniziale = name[0].toUpperCase();
+    iniziali.push(iniziale);
+    }
+    return iniziali 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(iniziali(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
